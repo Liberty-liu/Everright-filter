67b9382d68c398e9629f50437f3d7e6176ef03f6
Liberty <zhuizhuidea@gmail.com>
2023-07-27 14:27:56 +0800

    test: renderType: TEXT
    operator: "style=one_of" && value is not empty

--- test/linear/TEXT.test.js
+++ test/linear/TEXT.test.js
@@ -78,6 +78,39 @@ describe('renderType: TEXT', () => {
     expect(inputElm.classes()).toContain('ERFILTER-ERROR')
     nativeInput.value = '1'
     await inputWrapper.trigger('input')
-    expect(wrapper.findComponent({ ref: 'ERfilterRef' }).vm.getData(), '', 2).toMatchSnapshot()
+    expect(wrapper.findComponent({ ref: 'ERfilterRef' }).vm.getData()).toMatchSnapshot()
+  })
+  test.only('operator: "style=one_of" && value is not empty', async () => {
+    await wrapper.find(utils.getTestId(`${NAME.PICKERCOMPONENT}-0-0`)).find('.Everright-filter-TriggerComponent').trigger('click')
+    document.querySelector(utils.getTestId(`${NAME.TRIGGERCOMPONENT}-popperClass`, 'id')).querySelectorAll('.el-cascader-node')[0].click()
+    await nextTick()
+    document.querySelector(utils.getTestId(`${NAME.TRIGGERCOMPONENT}-popperClass`, 'id')).querySelectorAll('.el-cascader-menu')[1].querySelectorAll('.el-cascader-node')[5].click()
+    await flushPromises()
+    await wrapper.find(utils.getTestId(`${NAME.PICKERCOMPONENT}-0-0`)).find(utils.getTestId('operator', 'id')).trigger('click')
+    const selectOptions = getSelectOptions(utils.getTestId('operator-popperClass', 'id'))
+    expect(selectOptions.map(e => utils.getAttrs(e).value)).toEqual([
+      'equal',
+      'one_of',
+      'not_equal',
+      'contains',
+      'not_contain',
+      'empty',
+      'not_empty'
+    ])
+    selectOptions[1].click()
+    await nextTick()
+    let selectElm = wrapper.find(utils.getTestId(`${NAME.PICKERCOMPONENT}-0-0`)).find(utils.getTestId(`${NAME.TEXTTYPE}-select`, 'id'))
+    expect(wrapper.findComponent({ ref: 'ERfilterRef' }).vm.getData()).toEqual({})
+    await new Promise(resolve => setTimeout(resolve, 100))
+    selectElm = wrapper.find(utils.getTestId(`${NAME.PICKERCOMPONENT}-0-0`)).find(utils.getTestId(`${NAME.TEXTTYPE}-select`, 'id'))
+    expect(selectElm.classes()).toContain('ERFILTER-ERROR')
+    const inputWrapper = selectElm.find('.el-select__input')
+    const selectInputEl = inputWrapper.element
+    selectInputEl.value = '1'
+    vi.useFakeTimers()
+    await inputWrapper.trigger('input')
+    getSelectOptions(utils.getTestId(`${NAME.TEXTTYPE}-select-popperClass`, 'id'))[0].click()
+    await nextTick()
+    expect(wrapper.findComponent({ ref: 'ERfilterRef' }).vm.getData()).toMatchSnapshot()
   })
 })
